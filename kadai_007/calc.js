1fb84622aa3af4901053d250b4f8ef5b55f13485
hamayuki0212 <yu.0212.ha@icloud.com>
2024-07-29 19:20:56 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,24 @@
+//変数numに0~15までのランダムな整数を代入する
+let num=Math.floor(Math.random() * 16);
+
+// 変数numの値を出力する
+console.log(num);
+
+// 変数numの値によって、出力する文字列を切り替える
+if(num %15 ===0){
+  console.log('3と5の倍数です');
+}
+
+else if(num %3===0){
+  console.log('3の倍数です');
+}
+
+else if(num %5===0){
+  console.log('5の倍数です');
+}
+
+//それ以外の場合は変数numを出力する
+else{
+  console.log(num)
+}
+
